578a4055ade7477695ea7c54b188ecea9f6ff5b5
Sunbreak <sunbreak.wang@gmail.com>
2020-12-31 18:38:58 +0800

    Upgrade from flutter-1.12.13 to flutter-1.22.5 for Android
    Signed-off-by: Sunbreak <sunbreak.wang@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -26,6 +28,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
